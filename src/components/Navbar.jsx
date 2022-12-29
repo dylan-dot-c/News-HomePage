7329667bddd8975fdb458156334f0cd779b4f418
Dylan Heslop <heslopd23@gmail.com>
2022-12-29 08:07:40 -0800

    added some more images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 
 import "../styles/navbar.css"
 import logo from "../assets/images/logo.svg"
+import menu from "../assets/images/icon-menu.svg"
+import close from "../assets/images/icon-menu-close.svg"
+
 
 export default function Navbar() {
 
@@ -15,7 +18,7 @@ export default function Navbar() {
     return (
         <nav>
             <img src={logo} width="50px" />
-            <img onClick={toggle} className="menu--icon" src="/src/assets/images/icon-menu.svg" width="50px" />
+            <img onClick={toggle} className="menu--icon" src={menu} width="50px" />
 
             <div className={isExpanded ? "menu--container menu--show": "menu--container"}>
                 <ul className="menu--expanded">
@@ -24,7 +27,7 @@ export default function Navbar() {
                     <li>Popular</li>
                     <li>Trending</li>
                     <li>Categories</li>
-                    <li onClick={toggle} id="closeNav"> <img src="/src/assets/images/icon-menu-close.svg" />  </li>
+                    <li onClick={toggle} id="closeNav"> <img src={close} />  </li>
                 </ul>
             </div>
         </nav>
